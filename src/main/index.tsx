9430cb558a2969a46c7bcfed06a690f80b69c69a
Ui Sam <uisam.santos@gmail.com>
2023-11-26 17:33:44 -0300

    fix: move global styles to index

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,5 +1,6 @@
 import { Router } from '@/presentation/components'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@/presentation/styles/global.scss'
 
 ReactDOM.createRoot(document.getElementById('main')!).render(<Router />)
