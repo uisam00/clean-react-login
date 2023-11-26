905a1f7ee5549daf419c4f0c425078a634958d06
Ui Sam <uisam.santos@gmail.com>
2023-11-26 16:49:32 -0300

    feat: add app router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,5 +1,5 @@
-import { Login } from '@/presentation/pages'
+import { Router } from '@/presentation/components'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('main')!).render(<Login />)
+ReactDOM.createRoot(document.getElementById('main')!).render(<Router />)
