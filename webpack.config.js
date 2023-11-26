b04789c2927f40f0fe0a772cba0512869737d5ec
Ui Sam <uisam.santos@gmail.com>
2023-11-26 14:52:12 -0300

    chore: fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -42,8 +42,10 @@ module.exports = {
     ],
   },
   devServer: {
-    contentBase: './public',
-    writeToDisk: true,
+    static: './public',
+    devMiddleware: {
+      writeToDisk: true,
+    },
     historyApiFallback: true,
   },
   externals: {
