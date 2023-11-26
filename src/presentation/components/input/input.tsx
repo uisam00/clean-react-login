6f8d94d89edef7e875788df63763e61ed70d764a
Ui Sam <uisam.santos@gmail.com>
2023-11-26 17:14:01 -0300

    feat: remove autofill on browser

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -4,9 +4,13 @@ import Styles from './input-styles.scss'
 type Props = React.InputHTMLAttributes<HTMLInputElement>
 
 const Input: React.FC<Props> = (props) => {
+  const enabledInput = (event: React.FocusEvent<HTMLInputElement>): void => {
+    event.target.readOnly = false
+  }
+
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} />
+      <input {...props} onFocus={enabledInput} />
       <span className={Styles.status}>🔴</span>
     </div>
   )
