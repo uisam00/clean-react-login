e8aee094b06db4ed2611ba92c111e52f1a0cc94c
Ui Sam <uisam.santos@gmail.com>
2024-01-19 22:57:16 -0300

    test: prevent Login to call submit when loading

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -144,4 +144,13 @@ describe('Login Component', () => {
 
     expect(authenticationSpy.params).toEqual({ email, password })
   })
+
+  test('Should call Authentication only once', () => {
+    const { sut, authenticationSpy } = makeSut()
+
+    simulateValidSubmit(sut)
+    simulateValidSubmit(sut)
+
+    expect(authenticationSpy.callsCount).toBe(1)
+  })
 })
