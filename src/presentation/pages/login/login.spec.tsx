73e37d3aba125a74b8e111542fe4321a46888a26
Ui Sam <uisam.santos@gmail.com>
2024-01-19 23:50:07 -0300

    test: ensure login navigates to signup correctly

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+
 import faker from 'faker'
 import 'jest-localstorage-mock'
 import { RenderResult, render, fireEvent, cleanup, waitFor } from '@testing-library/react'
@@ -15,13 +18,19 @@ type SutParams = {
   validationError: string
 }
 
+const history = createMemoryHistory()
+
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub()
   const authenticationSpy = new AuthenticationSpy()
 
   validationStub.errorMessage = params?.validationError
 
-  const sut = render(<Login validation={validationStub} authentication={authenticationSpy} />)
+  const sut = render(
+    <Router history={history}>
+      <Login validation={validationStub} authentication={authenticationSpy} />
+    </Router>,
+  )
   return {
     sut,
     authenticationSpy,
@@ -192,5 +201,27 @@ describe('Login Component', () => {
     await waitFor(() => sut.getByTestId('form'))
 
     expect(localStorage.setItem).toHaveBeenCalledWith('accessToken', authenticationSpy.account.accessToken)
+  })
+
+  test('Should go to signup page', () => {
+    const { sut } = makeSut()
+
+    const signup = sut.getByTestId('signup')
+
+    fireEvent.click(signup)
+
+    expect(history.length).toBe(2)
+    expect(history.location.pathname).toBe('/signup')
+  })
+
+  test('Should go to signup page', () => {
+    const { sut } = makeSut()
+
+    const signup = sut.getByTestId('signup')
+
+    fireEvent.click(signup)
+
+    expect(history.length).toBe(2)
+    expect(history.location.pathname).toBe('/signup')
   })
 })
