f6a722b6ca794ba0689425b7479d67bf6c273de3
Ui Sam <uisam.santos@gmail.com>
2024-01-20 11:31:39 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,12 +1,19 @@
 import { InvalidFieldError } from '@/validation/errors'
 import { EmailValidation } from './email-validation'
 
+import faker from 'faker'
+
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const fieldName = 'email'
+    const fieldName = faker.database.column()
     const sut = new EmailValidation(fieldName)
-    const error = sut.validate('')
-
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError(fieldName))
+  })
+
+  test('Should return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.database.column())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
