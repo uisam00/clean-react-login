3b7a1aebf0bb5fec616e75b5e13596bc669174c8
Ui Sam <uisam.santos@gmail.com>
2024-01-20 10:45:36 -0300

    feat: ensure RequiredFieldValidation return falsy if field is not empty

--- src/validation/required-field/required-field-validation.ts
+++ src/validation/required-field/required-field-validation.ts
@@ -4,8 +4,7 @@ import { RequiredFieldError } from '@/validation/errors'
 export class RequiredFieldValidation implements FieldValidation {
   constructor(readonly filed: string) {}
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   validate(value: string): Error {
-    return new RequiredFieldError()
+    return value ? null : new RequiredFieldError()
   }
 }
