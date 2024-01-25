23bc07a956defaf5c105e3ef424173114fc2d619
Ui Sam <uisam.santos@gmail.com>
2024-01-24 21:14:22 -0300

    test: ensure ValidationComposite return falsy if no validators fail

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,28 +1,31 @@
 import { FieldValidationSpy } from '@/validation/test/mock-field-validation'
 import { ValidationComposite } from './validation-composite'
+import faker from 'faker'
 
 type SutTypes = {
   sut: ValidationComposite
   fieldValidationsSpy: FieldValidationSpy[]
+  fieldName: string
 }
-const makeSut = (): SutTypes => {
-  const fieldValidationsSpy = [new FieldValidationSpy('any_field'), new FieldValidationSpy('any_field')]
-
+const makeSut = (fieldName = faker.database.column()): SutTypes => {
+  const fieldValidationsSpy = [new FieldValidationSpy(fieldName), new FieldValidationSpy(fieldName)]
   const sut = new ValidationComposite(fieldValidationsSpy)
-
-  return { sut, fieldValidationsSpy }
+  return { sut, fieldValidationsSpy, fieldName }
 }
 
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const { sut, fieldValidationsSpy } = makeSut()
+    const { sut, fieldValidationsSpy, fieldName } = makeSut()
+    const errorMessage = faker.random.words()
+    fieldValidationsSpy[0].error = new Error(errorMessage)
+    fieldValidationsSpy[1].error = new Error(faker.random.words())
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBe(errorMessage)
+  })
 
-    fieldValidationsSpy[0].error = new Error('first_error_message')
-
-    fieldValidationsSpy[1].error = new Error('second_error_message')
-
-    const error = sut.validate('any_field', 'any_value')
-
-    expect(error).toBe('first_error_message')
+  test('Should return falsy if no validators fail', () => {
+    const { sut, fieldName } = makeSut()
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
