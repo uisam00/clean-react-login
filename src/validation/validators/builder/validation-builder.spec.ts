85381bad2b6728db69446544f612b37ca172e268
Ui Sam <uisam.santos@gmail.com>
2024-01-24 21:53:06 -0300

    test: ensure ValidationBuilder return a list of validators

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,20 +1,35 @@
 import { EmailValidation, RequiredFieldValidation, MinLengthValidation } from '@/validation/validators'
 import { ValidationBuilder as sut } from './validation-builder'
+import faker from 'faker'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
-    const validation = sut.field('any_field').required().build()
-    expect(validation).toEqual([new RequiredFieldValidation('any_field')])
+    const filedName = faker.database.column()
+    const validators = sut.field(filedName).required().build()
+    expect(validators).toEqual([new RequiredFieldValidation(filedName)])
   })
 
   test('Should return EmailValidation', () => {
-    const validation = sut.field('any_field').email().build()
-    expect(validation).toEqual([new EmailValidation('any_field')])
+    const filedName = faker.database.column()
+    const validators = sut.field(filedName).email().build()
+    expect(validators).toEqual([new EmailValidation(filedName)])
   })
 
   test('Should return MinLengthValidation', () => {
-    const minLength = 3
-    const validation = sut.field('any_field').min(minLength).build()
-    expect(validation).toEqual([new MinLengthValidation('any_field', minLength)])
+    const filedName = faker.database.column()
+    const length = faker.random.number()
+    const validators = sut.field(filedName).min(length).build()
+    expect(validators).toEqual([new MinLengthValidation(filedName, length)])
+  })
+
+  test('Should return a list of validators', () => {
+    const filedName = faker.database.column()
+    const length = faker.random.number()
+    const validators = sut.field(filedName).required().min(length).email().build()
+    expect(validators).toEqual([
+      new RequiredFieldValidation(filedName),
+      new MinLengthValidation(filedName, length),
+      new EmailValidation(filedName),
+    ])
   })
 })
