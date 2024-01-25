32b001261b62917063e94d9abfdbeb382c0bf793
Ui Sam <uisam.santos@gmail.com>
2024-01-24 21:36:19 -0300

    test: ensure ValidationBuilder hash email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,9 +1,14 @@
-import { RequiredFieldValidation } from '@/validation/validators'
+import { EmailValidation, RequiredFieldValidation } from '@/validation/validators'
 import { ValidationBuilder as sut } from './validation-builder'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
     const validation = sut.field('any_field').required().build()
     expect(validation).toEqual([new RequiredFieldValidation('any_field')])
+  })
+
+  test('Should return EmailValidation', () => {
+    const validation = sut.field('any_field').email().build()
+    expect(validation).toEqual([new EmailValidation('any_field')])
   })
 })
