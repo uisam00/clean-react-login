8ae432730650419edb5b19e5e256785ff0bacb50
Ui Sam <uisam.santos@gmail.com>
2024-01-25 21:19:37 -0300

    test: ensure ValidationComposite use correct validation

---
+++ src/main/factories/pages/login/login-validation/login-validation-factory.spec.ts
@@ -0,0 +1,14 @@
+import { ValidationBuilder as Builder, ValidationComposite } from '@/validation/validators'
+import { makeLoginValidation } from './login-validation-factory'
+
+describe('LoginValidationFactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeLoginValidation()
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...Builder.field('email').required().email().build(),
+        ...Builder.field('password').required().min(5).build(),
+      ]),
+    )
+  })
+})
